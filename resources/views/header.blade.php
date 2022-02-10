41e1116d3b5ea75a8c9b4b0ca6a04f2f165b6ef5
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-02-10 15:36:16 +0100

    finish bonus

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,10 +1,46 @@
+<style scoped>
+    header {
+        text-align: center;
+    }
+
+    header nav {
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+    }
+
+    header nav ul {
+        display: flex;
+        list-style: none;
+        padding: 0;
+    }
+
+    ul li {
+        padding: 0 1em;
+    }
+
+    li a {
+        text-decoration: none;
+        color: blue;
+    }
+
+    li a:hover {
+        cursor: pointer;
+        color: red;
+    }
+
+</style>
+
 <header>
+    <h2>
+        My Men&ugrave;
+    </h2>
     <nav>
         <ul>
             <li><a href="/">Welcome!</a></li>
-            <li><a href="{{route('andrea')}}">Andrea</a></li>
-            <li><a href="{{route('simone')}}">Simone</a></li>
-            <li><a href="{{route('lenticchie')}}">Lenticchie</a></li>
+            <li><a href="{{ route('andrea') }}">Andrea</a></li>
+            <li><a href="{{ route('simone') }}">Simone</a></li>
+            <li><a href="{{ route('lenticchie') }}">Lenticchie</a></li>
         </ul>
     </nav>
 </header>
